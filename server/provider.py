59d733bfcce8c4a8ba1a1ec0fdcb03d1b3490a57
Florent Espanet <florent@lumapps.com>
2016-02-20 11:40:00 +0100

    Add base provider class

---
+++ server/provider.py
@@ -0,0 +1,22 @@
+class Provider(object):
+    """Base provider class"""
+    def __init__(self, key=''):
+        self.key = key
+
+    def create(self):
+        raise NotImplemented()
+
+    def start(self):
+        raise NotImplemented()
+
+    def stop():
+        raise NotImplemented()
+
+    def dstroy():
+        raise NotImplemented()
+
+    def list_servers():
+        raise NotImplemented()
+
+    def status():
+        raise NotImplemented()
