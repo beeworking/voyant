dcae27f4232895bb0a139beeaf4f1d8da3b506e9
Yann Vaillant <yann@vaillant.im>
2016-02-20 13:45:40 +0100

    Use our dockvpn fork

--- scripts/setup-openvpn.sh
+++ scripts/setup-openvpn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -d --privileged --name openvpn-docker -p 1194:1194/udp -p 443:443/tcp beeworking/dockvpn
-docker run -d -p 8080:8080 --name serve-config --volumes-from openvpn-docker beeworking/dockvpn serveconfig
+docker run -d --privileged --name openvpn-docker -p 1194:1194/udp -p 443:443/tcp vayan/dockvpn
+docker run -d -p 8080:8080 --name serve-config --volumes-from openvpn-docker vayan/dockvpn serveconfig
